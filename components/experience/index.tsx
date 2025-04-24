084896d112aa3b5e72eb128a0ae7d9acdeb694c3
George Nick Gorzynski <georgegorzynski@me.com>
2025-04-24 12:50:31 +0100

    Add id prop to sections to enable jumping to sections

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -6,7 +6,7 @@ export default function Experience() {
 
   return (
     <div className={"container mx-auto pt-16"}>
-      <h2 className={"font-mono font-medium text-2xl mt-8"}>My Experience</h2>
+      <h2 id={"experience"} className={"font-mono font-medium text-2xl mt-8"}>My Experience</h2>
       <div className={"mt-8 flex flex-col gap-4"}>
         {experience.map((xp) => <ExperienceCard key={xp.slug} experience={xp} />)}
       </div>
--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -6,7 +6,7 @@ export default function Projects() {
 
   return (
     <div className={"container mx-auto pt-16"}>
-      <h2 className={"font-mono font-medium text-2xl mt-8"}>My Projects</h2>
+      <h2 id={"projects"} className={"font-mono font-medium text-2xl mt-8"}>My Projects</h2>
       <div className={"mt-8 grid grid-cols-1 md:grid-cols-2 gap-4"}>
         {projects.map((project) => <ProjectCard key={project.slug} project={project} />)}
       </div>
